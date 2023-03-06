c50cccb236af533a46dd71cd22a9713b3ba13fa6
sridhar vumma <50115187+sridatascience@users.noreply.github.com>
2023-03-06 13:42:57 +0530

    Updated with SFR new register types

--- dSPIC33CK256MP508_AutomatedTest.X/newmain.c
+++ dSPIC33CK256MP508_AutomatedTest.X/newmain.c
@@ -1,17 +1,17 @@
 #include <xc.h>
 #pragma config ICS = PGD3              // ICD Communication Channel Select bits (Communicate on PGC3 and PGD3)
-uint8_t m =10;
+volatile uint8_t m =10;
 uint8_t add(uint8_t n, uint8_t o, uint8_t p ){
    return n+o+p;
 }
 void main() {
-   volatile uint8_t a = 0x01;
-   PG2CONLbits.ON = a;
-   volatile uint8_t c = 0x01;
-   PG4CONLbits.ON = c;
-   volatile uint8_t b =0x01;
-   PG1CONLbits.ON = b;
-   volatile uint8_t t = add(a,b,c)-0x03;
-   CORCONbits.SATA = t;
+   volatile uint8_t a = 0x05;
+   POS1CNTH = a;
+   volatile uint8_t c = 0x1F;
+   POS1CNTL = c;
+   volatile uint8_t b =0x18;
+   PR1= b;
+   volatile uint8_t t = add(a,b,c);
+   TMR1 = t;
    return;
 }
