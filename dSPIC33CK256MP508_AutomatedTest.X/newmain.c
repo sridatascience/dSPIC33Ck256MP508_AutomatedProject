316fa257f380c87d4456953da0153c8ddf01cdda
sridhar vumma <50115187+sridatascience@users.noreply.github.com>
2023-05-18 16:31:37 +0530

    Update newmain.c
    updated dspic33ck256mp508

--- dSPIC33CK256MP508_AutomatedTest.X/newmain.c
+++ dSPIC33CK256MP508_AutomatedTest.X/newmain.c
@@ -6,13 +6,13 @@ uint8_t add(uint8_t n, uint8_t o, uint8_t p ){
 }
 void main() {
    volatile uint8_t a = 0x05;
-   POS1CNTH = a;
-   m = POS1CNTH;
+   TMR1 = a;
+   m = TMR1;
    volatile uint8_t c = 0x1F;
    POS1CNTL = c;
    volatile uint8_t b =0x08;
    PR1= b;
    volatile uint8_t t = add(a,b,c);
-   TMR1 = t;
+   POS1CNTH = t;
    return;
 }
